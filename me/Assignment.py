dbc496a08864a123603f2be15245da2183913c19
duyisong <duyisong@bilibili.com>
2019-07-03 18:41:43 +0800

    Initial commit

---
+++ me/Assignment.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 赋值
+
+name = "will"  # 字符串
+age = 20  # 整型
+a = b = c = 1  # 多个变量赋值
+
+x, y, z = 1, 2, 'john'  # x=1 y=2 z='john'
+
+print "name:" + name
+print "age:" + str(age)  # str将int转为string
+print "a:" + str(a)
+print "x:" + str(x)
+print "z:" + z
