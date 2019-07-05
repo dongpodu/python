abe79d955ed747bc48d5fb8e97ac1f5b391b89db
duyisong <duyisong@bilibili.com>
2019-07-05 10:36:16 +0800

    test

--- me/For.py
+++ me/For.py
@@ -20,6 +20,7 @@ print range(10)  # 定义从0-10的有序序列，步长为1
 print range(10, 20)  # 定义从10-20的有序序列，步长为1
 print range(10, 20, 2)  # 定义从10-20的有序序列，步长为2
 
+print '----------4------------'
 # 在 python 中，for … else 表示这样的意思，for 中的语句和普通的没有区别，else 中的语句会在循环正常执行完（即 for 不是通过 break 跳出而中断的）的情况下执行，while … else 也是一样
 for index in range(1, 10):
     print '执行for循环'
