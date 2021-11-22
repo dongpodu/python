e89047b6d2cafd09e453da8d9903f74ea9b96c8c
duyisong <duyisong@bilibili.com>
2021-11-22 18:49:26 +0800

    test

---
+++ me/method/defMethod.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+def my_abs(x):  # 自定义函数
+    return 1
+
+
+def m1(x):  # 没有返回值的函数，默认返回None，等同于return None
+    print("m1函数执行....")
+
+
+def returnMutiValue(x, y):
+    return x, y  # 看上去返回的是多个值，其实是一个tuple对象，语法可以这么简写
+
+
+def m2(x, y=2, z="name"):  # y默认等于1，外部调用时可以不传n参数
+    print("x is %s" % x)
+    print("y is %s" % y)
+    print("z is %s" % z)
+
+
+m2(3)
+m2(3, 4)  # 按顺序写参数，不需要指定名
+m2(3, z='name1')  # 不按顺序写参数，需指定参数名
---
+++ me/method/invoke.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+from defMethod import my_abs
+from defMethod import m1
+from defMethod import returnMutiValue
+
+print(str(1))
+print(bool(1))
+print(int(1.2))
+
+a = abs  # 函数名其实就是指向一个函数对象的引用，完全可以把函数名赋给一个变量，相当于给这个函数起了一个“别名”
+print(a(-1))
+
+print(my_abs(2000))
+print(m1(2000))
+
+print(isinstance(1, (str, float)))  # 判断1是否是类型str或者float
+
+print(returnMutiValue(1, 3))
